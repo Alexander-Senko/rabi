698b6e2cde182358c9cb3624a28c0eeaad24d4df
Alexander Senko <Alexander.Senko@gmail.com>
2013-08-09 19:55:02 +0700

    Fixed `User#name` when e-mail is absent.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
 	def name
-		case (name = email.split('@').first.gsub('.', ' ')).size
-			when 2..3 then name.upcase
-			else           name.titleize
-		end
+		name = email.split('@').first and
+			case (name.gsub('.', ' ')).size
+				when 2..3 then name.upcase
+				else           name.titleize
+			end
 	end unless table_exists? and 'name'.in? column_names
 end
